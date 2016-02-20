19358001fc710a54a338e4332ac93b3faef77ac6
Kiba Fox <kiba.fox@foxypossibilities.com>
2016-02-20 18:58:54 -0500

    Set environment variables with profile and config.fish and add GOPATH
    The pam_environment method is too limited.  I can't use variables like
    $HOME, so I decided on switching to setting my environment variables in
    config.fish and profile.  This means some duplication so they are
    available in fish and bash, but less frustrations trying to get
    pam_environment to work in all cases.

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,7 @@
 # Turn off greeting on start
 set -g -x fish_greeting ''
 
-alias vim nvim
+# Environment Variables
+set -x EDITOR nvim
+set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
+set -x GOPATH "$HOME/gocode"
---
+++ profile
@@ -0,0 +1,3 @@
+export EDITOR=nvim
+export NVIM_TUI_ENABLE_TRUE_COLOR=1
+export GOPATH=$HOME/gocode
