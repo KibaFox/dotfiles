8887792e1d69a226204240faa297222a90042e97
Kiba Fox <kiba.fox@foxypossibilities.com>
2016-09-12 13:06:09 -0400

    Add an alias for xfreerdp with default configuration

--- fish/config.fish
+++ fish/config.fish
@@ -6,4 +6,7 @@ set -x EDITOR nvim
 set -x NVIM_TUI_ENABLE_TRUE_COLOR 1
 set -x GOPATH "$HOME/gocode"
 
+# Aliases
+alias rdp "xfreerdp +compression +clipboard +fonts /cert-ignore /size:1400x1050"
+
 source ~/.config/fish/fish_prompt.fish
