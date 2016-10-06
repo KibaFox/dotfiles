d0b27d8d5db7bf74b933a425c1ed2d95b4f324a6
Kiba Fox <kiba.fox@foxypossibilities.com>
2016-10-06 10:28:26 -0400

    Refine patterns for TinyFugue highlights
    I've come across usernames with single quotes and hyphens now.  I'm
    going to match any character that isn't a space.
    
    I noticed that I don't need a capturing group to do the OR logic of my
    regexp with a pipe (|).  This makes my regexp for detecting OOC simpler.
    
    I also noticed system messages with ## don't always have a space
    afterwards so I removed the space.

--- tinyfugue/config.tf
+++ tinyfugue/config.tf
@@ -5,26 +5,26 @@
 ; ------------
 
 ; Pages
-/def -p2 -aCcyan -P1B -t"^([\w']+) pages,?" hl_page1
+/def -p2 -aCcyan -P1B -t"^([^\s]+) pages,?" hl_page1
 /def -p2 -aCcyan -P1B -t"^In (?:a )?page-pose to you,? ([\w']+)" hl_page2
 /def -p2 -aCcyan -mregexp -t'^You page,?' hl_page3
 /def -p2 -aCcyan -mregexp -t'^You page-pose,?' hl_page4
 
 ; Whispers
-/def -p2 -aCblue -P1B     -t"^([\w']+) whispers,?" hl_whisper1
+/def -p2 -aCblue -P1B     -t"^([^\s]+) whispers,?" hl_whisper1
 /def -p3 -aCblue -mregexp -t'^You whisper,?' hl_whisper2
 
 ; Looks, smells, etc
-/def -p5 -aCmagenta -P1B -t"([\w']+) (?:just )?looked at you" hl_look1
-/def -p5 -aCmagenta -P1B -t"([\w']+) (?:just )?smell(?:s|ed) you" hl_smell1
+/def -p5 -aCmagenta -P1B -t"([^\s]+) (?:just )?looked at you" hl_look1
+/def -p5 -aCmagenta -P1B -t"([^\s]+) (?:just )?smell(?:s|ed) you" hl_smell1
 
 ; OOC
-/def -p14 -P0Cyellow -t'(?:<OOC>|\(OOC\))' hl_ooc1
+/def -p14 -P0Cyellow -t'<OOC>|\(OOC\)' hl_ooc1
 /def -p14 -P0Cyellow -t'^OOC:' hl_ooc2
 
 ; Events (arriving, system messages, etc)
 /def -p15 -aCgray -mregexp -t'^>> ' hl_events1
-/def -p15 -aCgray -mregexp -t'^## ' hl_events2
+/def -p15 -aCgray -mregexp -t'^##' hl_events2
 
 ; Local Configuration
 ; -------------------
