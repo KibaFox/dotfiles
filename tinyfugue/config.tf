72aac53c77e275410126d8c0ecf99bc3a5e09177
Kiba Fox <kiba.fox@foxypossibilities.com>
2016-10-13 15:14:35 -0400

    Better username pattern matching for TinyFugue
    Match anything that's not a space, colon, semi-colon, comma, or period.

--- tinyfugue/config.tf
+++ tinyfugue/config.tf
@@ -5,18 +5,18 @@
 ; ------------
 
 ; Pages
-/def -p2 -abCcyan -P1B -t"^([^\s]+) pages,?" hl_page1
-/def -p2 -abCcyan -P1B -t"^In (?:a )?page-pose to you,? ([\w']+)" hl_page2
+/def -p2 -abCcyan -P1B -t"^([^\s:;,.]+) pages,?" hl_page1
+/def -p2 -abCcyan -P1B -t"^In (?:a )?page-pose to you,? ([^\s:;,.]+)" hl_page2
 /def -p2 -aCcyan -mregexp -t'^You page,?' hl_page3
 /def -p2 -aCcyan -mregexp -t'^You page-pose,?' hl_page4
 
 ; Whispers
-/def -p2 -abCblue -P1B     -t"^([^\s]+) whispers,?" hl_whisper1
+/def -p2 -abCblue -P1B     -t"^([^\s:;,.]+) whispers,?" hl_whisper1
 /def -p3 -aCblue -mregexp -t'^You whisper,?' hl_whisper2
 
 ; Looks, smells, etc
-/def -p5 -abCmagenta -P1B -t"([^\s]+) (?:just )?looked at you" hl_look1
-/def -p5 -abCmagenta -P1B -t"([^\s]+) (?:just )?smell(?:s|ed) you" hl_smell1
+/def -p5 -abCmagenta -P1B -t"([^\s:;,.]+) (?:just )?looked at you" hl_look1
+/def -p5 -abCmagenta -P1B -t"([^\s:;,.]+) (?:just )?smell(?:s|ed) you" hl_smell1
 
 ; OOC
 /def -p14 -P0Cyellow -t'<OOC>|\(OOC\)' hl_ooc1
