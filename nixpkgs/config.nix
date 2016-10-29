4059b7396be6633ed5fa0974c7d6bd79b19d841e
Kiba Fox <kiba.fox@foxypossibilities.com>
2016-10-29 11:36:52 -0400

    Enable flash and vine in chromium

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -7,6 +7,11 @@
   # Verify new pkgs with this on before submitting a pull request to nixpkgs
   #build-use-sandbox = true;
 
+  chromium = {
+    enablePepperFlash = true;       # Some sites still need flash
+    enableWideVine = true;          # Allows Netflix to be played on Linux
+  };
+
   packageOverrides = pkgs : rec {
 
     st = with pkgs; pkgs.st.override {
