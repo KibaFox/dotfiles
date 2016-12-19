12f9343b07d08988963621121310556df417749b
Kiba Fox <kiba.fox@foxypossibilities.com>
2016-12-19 18:53:46 -0500

    Fish aliases to cleanup docker
    I updated the one to remove unused images to use the docker filter
    instead of `awk`.  I've added an alias for volumes and containers.

--- fish/config.fish
+++ fish/config.fish
@@ -16,7 +16,11 @@ alias rsyncg "rsync -a --exclude='.git/' --exclude-from='.gitignore'"
 alias clean-swp "rm -f ~/.local/share/nvim/swap/*.swp"
 
 # Cleanup unused docker images
-alias clean-dkr-img 'docker rmi (docker images | grep "^<none>" | awk \'{print $3}\')'
+alias clean-dkr-img 'docker rmi (docker images -f "dangling=true" -q)'
+# Cleanup unused docker volumes
+alias clean-dkr-vol 'docker volume rm (docker volume ls -qf dangling=true)'
+# Cleanup exited docker containers
+alias clean-dkr-con 'docker rm -v (docker ps -a -q -f status=exited)'
 
 # Tinyfugue
 # Don't auto-connect on start
