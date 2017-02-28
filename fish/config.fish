d7b9329b240faf5191267b99deb4cf33acd158cf
Kiba Fox <kiba.fox@foxypossibilities.com>
2017-02-28 09:31:49 -0500

    Remove st fix; this caused ?[?1h?= prefix in fzf

--- fish/config.fish
+++ fish/config.fish
@@ -36,18 +36,6 @@ alias clean-dkr-con 'docker rm -v (docker ps -a -q -f status=exited)'
 # Load config in xdg style
 alias tf "tf -n -f~/.config/tinyfugue/config.tf"
 
-# Fix the delete key when using st
-# https://github.com/fish-shell/fish-shell/issues/2139#issuecomment-137228149
-tput smkx ^/dev/null
-function fish_enable_keypad_transmit --on-event fish_postexec
-    tput smkx ^/dev/null
-end
-
-function fish_disable_keypad_transmit --on-event fish_preexec
-    tput rmkx ^/dev/null
-end
-# end of - Fix the delete key when using st
-
 # Allow extension via local configuration
 if test -f ~/.config/fish/config_local.fish
     source ~/.config/fish/config_local.fish
