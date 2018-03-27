71006f3c91314e4421978c5afd626819f56794af
Kiba Fox <kiba.fox@foxypossibilities.com>
2018-03-27 11:12:08 -0400

    Emacs: no need to have rich minority

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -78,22 +78,15 @@
   :after evil
 )
 
-; Hide minor modes on the mode-line
-; https://github.com/Malabarba/rich-minority
-(use-package rich-minority
-    :config
-    (setq rm-blacklist
-      (format "^ \\(%s\\)$"
-        (mapconcat #'identity
-          '("EditorConfig" "Undo-Tree")
-          "\\|")))
-    (rich-minority-mode 1)
-)
-
 ; https://github.com/Malabarba/smart-mode-line
 (use-package smart-mode-line
   :init
-    (setq sml/no-confirm-load-theme t)
+  (setq sml/no-confirm-load-theme t)
+  (setq rm-blacklist ; Hide some minior modes.
+    (format "^ \\(%s\\)$"
+      (mapconcat #'identity
+        '("EditorConfig" "Undo-Tree")
+        "\\|")))
   :config
     (sml/setup)
 )
