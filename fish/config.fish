ea764b489c84326cb50083e2e050a0871e742f99
Kiba Fox <kiba.fox@foxypossibilities.com>
2018-09-12 19:34:01 -0400

    fish: add dkr function with subcommands
    dkr is a utility for managing Docker.  The subcommands are:
    
    killall    to kill and remove all containers
    clean      to remove exited docker containers
    clean-img  to remove dangling images
    clean-vol  to remove dangling volumes
    
    This replaces some existing aliases.

--- fish/config.fish
+++ fish/config.fish
@@ -55,13 +55,6 @@ alias rsyncg "rsync -a --exclude='.git/' --exclude-from='.gitignore'"
 # Clean out neovim swp files
 alias clean-swp "rm -f ~/.local/share/nvim/swap/*.swp"
 
-# Cleanup unused docker images
-alias clean-dkr-img 'docker rmi (docker images -f "dangling=true" -q)'
-# Cleanup unused docker volumes
-alias clean-dkr-vol 'docker volume rm (docker volume ls -qf dangling=true)'
-# Cleanup exited docker containers
-alias clean-dkr-con 'docker rm -v (docker ps -a -q -f status=exited)'
-
 # Tinyfugue
 # Don't auto-connect on start
 # Load config in xdg style
---
+++ fish/functions/dkr.fish
@@ -0,0 +1,51 @@
+function dkr -d "Shortcuts for managing Docker"
+	function helptext
+		echo 'Usage: dkr <command>'
+		echo ''
+		echo 'Command may be:'
+		echo ''
+		echo 'killall	to kill and remove all containers'
+		echo 'clean	to remove exited docker containers'
+		echo 'clean-img	to remove dangling images'
+		echo 'clean-vol	to remove dangling volumes'
+		return 0
+	end
+
+	if test (count $argv) -le 0
+		helptext
+		return 0
+	end
+
+	set -l cmd $argv[1]
+	switch $cmd
+		case killall
+			set -l containers (docker ps -a -q)
+			if test -n "$containers"
+				docker rm -f $containers
+			end
+
+		case clean
+			set -l exited (docker ps -a -q -f status=exited)
+			if test -n "$exited"
+				docker rm -v $exited
+			end
+
+		case clean-img
+			set -l dangling (docker images -f "dangling=true" -q)
+			if test -n "$dangling"
+				docker rmi $dangling
+			end
+
+		case clean-vol
+			set -l dangling (docker volume ls -qf dangling=true)
+			if test -n "$dangling"
+				docker volume rm $dangling
+			end
+
+		case '*'
+			echo "Command $cmd not understood!"
+			echo ''
+			helptext
+			return 1
+	end
+end
