0559a5e2505d8630cab2c9ee1d4c027c09b8056f
Kiba Fox <kiba.fox@foxypossibilities.com>
2018-12-09 14:29:11 -0500

    Update weechat plugins

--- bin/install-tools.sh
+++ bin/install-tools.sh
@@ -4,7 +4,7 @@ set -ex
 # Install script to install tools and utilities commonly used.
 # Currently only supports MacOS and doesn't check things are already installed.
 
-weechat=$HOME/.weechat
+weechat=$HOME/.config/weechat
 # weechat (w/ support for matrix)
 brew install lua
 # Use a downgraded version of lua-cjson https://stackoverflow.com/a/50499755
@@ -17,10 +17,10 @@ pip install -U websocket-client
 mkdir -p $weechat/python $weechat/lua
 # download wee-slack
 curl -fsSo $weechat/python/wee_slack.py \
-	https://raw.githubusercontent.com/wee-slack/wee-slack/master/wee_slack.py
+	'https://raw.githubusercontent.com/wee-slack/wee-slack/v2.2.0/wee_slack.py'
 # download matrix
 curl -fsSo $weechat/lua/matrix.lua \
-	https://raw.githubusercontent.com/torhve/weechat-matrix-protocol-script/master/matrix.lua
+	'https://raw.githubusercontent.com/torhve/weechat-matrix-protocol-script/master/matrix.lua'
 
 # surfraw
 brew install surfraw
