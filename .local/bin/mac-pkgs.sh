b649c519b713dfc5a1e8a22f0a452a67e3d9c40b
Kiba Fox <kiba.fox@foxypossibilities.com>
2020-10-20 23:03:25 -0400

    mac-pkgs: slim down what I install

--- .local/bin/mac-pkgs.sh
+++ .local/bin/mac-pkgs.sh
@@ -13,51 +13,31 @@ fi
 brew upgrade --ignore-pinned
 
 brew install --quiet \
-	awscli \
 	bat \
-	dep \
-	dust \
 	fish \
 	git \
-	git-archive-all \
 	git-lfs \
 	go \
-	hashcat \
-	httpie \
-	jq \
-	libpq \
 	mage \
-	mitmproxy \
 	mosh \
-	mysql-client \
 	neovim \
-	packer \
 	pandoc \
 	pngquant \
-	protobuf \
 	pwgen \
 	ripgrep \
 	shellcheck \
 	tmux \
-	tree \
-	vault \
+	tree  \
 	;
 
 brew cask upgrade
 
 brew cask install \
-	blackhole \
 	dash \
-	docker \
 	drawio \
 	font-fira-code \
 	font-source-code-pro \
-	gimp \
-	gpg-suite \
-	keepassxc \
 	keepingyouawake \
-	prolific-pl2303 \
 	uhk-agent \
 	viscosity \
-	wireshark \
 	;
