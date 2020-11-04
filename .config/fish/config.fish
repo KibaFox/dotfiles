2714bf8879d816afbccdedbc9e4c5a853f13a4f4
Kiba Fox <kiba.fox@foxypossibilities.com>
2020-11-04 14:34:32 -0500

    fish: don't display date and command time

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,8 @@
 # ===============
 # bobthefish prompt options
 set -g theme_color_scheme gruvbox
-#set -g theme_display_date no
-#set -g theme_display_cmd_duration no
+set -g theme_display_date no
+set -g theme_display_cmd_duration no
 
 # Environment Variables
 # =====================
