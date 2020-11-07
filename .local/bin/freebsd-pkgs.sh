c306eb73fdf704aa8e5a1d799099b4ab1a42a2e5
Kiba Fox <kiba.fox@foxypossibilities.com>
2020-11-07 15:39:18 -0500

    freebsd: reduce packages
    Trying to minimize number of dependencies installed.

--- .local/bin/freebsd-pkgs.sh
+++ .local/bin/freebsd-pkgs.sh
@@ -1,17 +1,10 @@
 #!/bin/sh -eux
 
 pkg install \
-	bash \
-	bat \
-	bind-tools \
-	fish \
-	git-lfs \
+	git-lite \
 	go \
 	mosh \
 	neovim \
-	py37-pynvim \
-	py37-ranger \
-	python \
 	pwgen \
 	ripgrep \
 	tmux \
