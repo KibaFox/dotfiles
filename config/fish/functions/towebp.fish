1c6707a5ad3a882f37e3683993494efd06b2cadf
Kiba Fox <kiba.fox@foxypossibilities.com>
2021-05-24 11:57:42 +0100

    towebp: add resize and dry-run options; use batch
    Added the --resize option to specify the geometry that gets passed to
    `gm convert` with the default being 1024x> which means to resize
    anything larger than 1024 wide to be 1024 pixels wide but keep the
    aspect ratio.
    
    Added the --dry-run option to print the commands being sent to `gm
    batch` without running.
    
    Switched to using `gm batch` to process multiple images.

--- config/fish/functions/towebp.fish
+++ config/fish/functions/towebp.fish
@@ -1,19 +1,23 @@
-function towebp -d 'To WebP. Convert an image to WebP using GraphcsMagick.'
+function towebp -d 'Converts images to WebP using GraphicsMagick batch process.'
 	function _help
-		echo 'Usage: towebp [options] FILES...'
+		echo 'Usage: towebp [options] IMAGES...'
 		echo ''
+		echo 'Converts images to WebP using GraphicsMagick batch process.'
 		echo 'Run without options to convert with default settings.'
 		echo ''
 		echo 'Options:'
 		echo ''
-		echo '-i, --icon        create an icon'
+		echo '-r, --resize      resize images to specified geometry.  Default is `1024x>`'
+		echo '                  which means resize to 1024 pixels wide if width is larger'
+		echo '                  and maintain aspect ratio.'
+		echo '-n, --dry-run     print the GraphicsMagick commands, but do not run'
 		echo '-h, --help        to show this help text'
 	end
 
-	argparse --name=towebp 'i/icon' 'h/help' -- $argv
+	argparse --name=towebp 'n/dry-run' 'r/resize=' 'h/help' -- $argv
 	or return
 
-	if test -n "$_flag_h"
+	if test -n "$_flag_help" -o -z "$argv"
 		_help
 		return
 	end
@@ -23,13 +27,23 @@ function towebp -d 'To WebP. Convert an image to WebP using GraphcsMagick.'
 		return 1
 	end
 
-	if test -n "$_flag_i"
-		echo 'Icon option not implemented yet!'
-		return 1
+	if test -z "$_flag_resize"
+		set _flag_resize '1024x>'
 	end
 
-	for file in $argv
-		set -l webp (string replace -i -r '\.(png|jpg|jpeg)$' '.webp' "$file")
-		gm convert "$file" -resize '1024x>' "$webp"
+	function _batch --inherit-variable _flag_resize
+		for file in $argv
+			set -l webp (string replace -i -r '\.(png|jpg|jpeg)$' '.webp' "$file")
+			or set -l webp "$file.webp"
+			echo "convert '$file' -resize '$_flag_resize' '$webp'"
+			echo "identify '$webp'"
+		end
+	end
+
+	if test -n "$_flag_dry_run"
+		_batch $argv
+	else
+		_batch $argv | gm batch -echo on -feedback on -stop-on-error on -
+		or return 1
 	end
 end
